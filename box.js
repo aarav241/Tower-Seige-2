915f7492d077be26228db8e93ad04db53814e17a
aarav241 <73211719+aarav241@users.noreply.github.com>
2020-12-04 13:46:23 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,39 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.5
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      if (this.body.speed < 8)
+    {
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(1);
+      stroke("black");
+      fill(53,209,211);
+      rect(0, 0, this.width, this.height);
+      pop();
+    } else {
+      World.remove(world,this.body);  
+      
+     // push();
+     // this.Visibility = this.Visibility - 68;
+      //tint(0,this.Visibility);  
+      //rect(this.body.position.x,this.body.position.y,50,50);
+      //pop();
+  
+      }
+    }
+  }
